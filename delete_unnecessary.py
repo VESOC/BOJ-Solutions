c00336b5e466751d444122ef65379f833dab1a74
coder38611 <54740236+coder38611@users.noreply.github.com>
2021-07-21 10:44:27 +0900

    Fixed Python Code for Binary File Removal

--- delete_unnecessary.py
+++ delete_unnecessary.py
@@ -1,5 +1,7 @@
 import os
 for root, dirs, files in os.walk(os.getcwd()):
+    if '.git' in root or '.vscode' in root:
+        continue
     for file in files:
         if file.startswith("._") or file.endswith(".out") or file.endswith('.txt') or '.' not in file:
             os.remove(os.path.join(root, file))
